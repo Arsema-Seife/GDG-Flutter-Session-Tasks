c5b927ee1ce56c8a0b539e524039911d2d595a8c
Arsema-Seife <arsemaseife137@gmail.com>
2025-12-01 18:57:56 -0800

    Added Week 1 tasks and patterns

---
+++ Month 1/Week 1/Task 4/Pattern 3.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  for (int i = 1; i <= 5; i++) {
+    int stars = 2 * i - 1; 
+    for (int j = 1; j <= stars; j++) {
+      stdout.write('*');
+    }
+    print('');
+  }
+}
